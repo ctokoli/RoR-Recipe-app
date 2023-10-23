ed9a640f28df639f1441cc1c6c10e5367f53882d
ctokoli <matumasie@gmail.com>
2023-10-23 17:12:13 +0000

    Add food.rb

---
+++ app/models/food.rb
@@ -0,0 +1,10 @@
+class Food < ApplicationRecord
+    belongs_to :user, class_name: "user", foreign_key: "user_id"
+    has_many :recipe_foods, foreign_key: :food_id
+
+    validates :name, presence: true
+    validates :measurement_unit, presence: true
+    validates :price, presence: true
+    validates :quantity, presence: true
+
+end
