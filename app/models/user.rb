5c9b2b60e929d53bc0d0960c7d960781cb9dc7b7
ctokoli <matumasie@gmail.com>
2023-10-23 17:18:58 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :foods,foreign_key: :user_id
-    has_many :recipes,foreign_key: :user_id
+  has_many :foods, foreign_key: :user_id
+  has_many :recipes, foreign_key: :user_id
 
-    validates :name, presence: true
+  validates :name, presence: true
 end
