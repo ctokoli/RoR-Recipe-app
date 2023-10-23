fea303b0cde69dec9a0ff462ed48018f7285b6ef
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-10-24 01:03:22 +0300

    Add routes configuration for food, recipe, users, and recipe_food

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'food#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :food, only: [:index, :new, :create] do
+    resources :recipe, only: [:index, :new, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :users, only: [:index, :show, :new, :create]
+  resources :recipe_food, only: [:index, :new, :create]
+  resources :recipe, only: [:index, :new, :create]
 end
