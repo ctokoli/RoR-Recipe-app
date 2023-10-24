764c22db186ca14274a928ce7dc237a5812edd64
ctokoli <matumasie@gmail.com>
2023-10-24 21:38:39 +0000

    Update food_controller.rb

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,7 +1,25 @@
 class FoodController < ApplicationController
   def index; end
 
+  def new
+    @food = Food.new
+    
+  end
+
   def create
-    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+    @user =  current_user
+    @food = Food.new(create_param)
+    @food.user = @user
+    if @food.save
+      flash[:notice] = 'Food was successfully created.'
+      redirect_to food_index_path
+    else
+      flash.now[:error] = 'Oops, something went wrong'
+      render :new
+    end
+  end
+  
+  def create_param
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
