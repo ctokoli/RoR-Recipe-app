c104946aa73cdc3d781597a7f815a8dde6381853
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-10-25 18:23:28 +0300

    Add sign_out at the Navbar and modify the file

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
   root 'food#index'
 
   resources :food, only: [:index, :new, :create, :destroy] do
     resources :recipe, only: [:index, :new, :create]
   end
-  
+
   resources :foods, only: [:index, :show, :new, :create, :destroy]
   resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
     resources :recipe_foods, only: [:show, :create, :new, :destroy]
   end
-  
+
   resources :users, only: [:index, :show, :new, :create, :destroy]
 
+end
 end
