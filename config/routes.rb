94c1f21bd12c38013cea8c849de3bf0ae2de8ae8
ctokoli <matumasie@gmail.com>
2023-10-25 09:34:01 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'food#index'
 
-  resources :food, only: [:index, :new, :create] do
+  resources :food, only: [:index, :new, :create, :destroy] do
     resources :recipe, only: [:index, :new, :create]
   end
 
