55ba8c3005d6b161466701d5a61f9b1c01ce0b98
ctokoli <matumasie@gmail.com>
2023-10-26 08:39:03 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   resources :food, only: [:index, :new, :create, :destroy] do
     resources :recipe, only: [:index, :new, :create]
   end
+
+  resources :shopping_lists, only: [:index] 
+  resources :public_recipes, only: [:index]
   
   resources :foods, only: [:index, :show, :new, :create, :destroy]
   resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
-    resources :recipe_foods, only: [:show, :create, :new, :destroy]
+    resources :recipe_foods, only: [:show, :new, :create, :destroy]
   end
   
   resources :users, only: [:index, :show, :new, :create, :destroy]
