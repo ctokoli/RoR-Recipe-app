1e3959e12b8777d7546286bb4778799e73f0ad11
ctokoli <matumasie@gmail.com>
2023-10-26 12:07:27 +0000

    Update public_recipes_controller.rb

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,13 +1,13 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.where(public: true).order(created_at: :desc)
+    @public_recipes = Recipe.where(public: true)
     @public_recipes.each do |recipe|
-     @food_recipe = recipe.recipe_foods
+      @food_recipe = recipe.recipe_foods
     end
-
-    @food_recipe.each do |food|
-      puts food.food.inspect
-     end
+    
+    # @food_recipe.each do |food|
+    #   puts food.food.inspect
+    #  end
     
   end 
 
