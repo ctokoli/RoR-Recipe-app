cfe564fe31d479ddecd82505aecf10ee7f81de28
ctokoli <matumasie@gmail.com>
2023-10-26 14:12:09 +0000

    Update shopping_list_controller.rb

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,5 +1,5 @@
 class ShoppingListController < ApplicationController
-  skip_before_action :authenticate_user! #, only: [:index]
+  skip_before_action :authenticate_user! # , only: [:index]
   def index
     @recipes = Recipe.find(params[:recipe_id])
     @recipe_foods = @recipes.recipe_foods
