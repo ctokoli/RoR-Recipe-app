6f2c3447cc0f38a9b5ee46f40013ef8a0be9feb5
ctokoli <matumasie@gmail.com>
2023-10-26 13:50:56 +0000

    Update public_recipes_controller.rb

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,4 +1,5 @@
 class PublicRecipesController < ApplicationController
+  skip_before_action :authenticate_user! #, only: [:index]
   def index
     @public_recipes = Recipe.where(public: true).order(created_at: :desc)
   end
