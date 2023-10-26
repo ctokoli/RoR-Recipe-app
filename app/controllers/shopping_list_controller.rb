6429d214446ed0308ae33c6f71201854509e5a98
ctokoli <matumasie@gmail.com>
2023-10-26 13:24:29 +0000

    Update shopping_list_controller.rb

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,6 +1,6 @@
 class ShoppingListController < ApplicationController
-    def index
-        @recipes = Recipe.find(params[:recipe_id])
-        @recipe_foods = @recipes.recipe_foods
-    end
+  def index
+    @recipes = Recipe.find(params[:recipe_id])
+    @recipe_foods = @recipes.recipe_foods
+  end
 end
