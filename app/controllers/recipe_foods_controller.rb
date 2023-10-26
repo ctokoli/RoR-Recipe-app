d7cdbedac502e71606e39dcdf3721ec699b2ae11
ctokoli <matumasie@gmail.com>
2023-10-26 08:39:02 +0000

    Add recipe_foods_controller.rb

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,25 @@
+class RecipeFoodsController < ApplicationController
+  def index 
+  end
+  
+  def new 
+    @recipe_food = RecipeFood.new
+    @recipe = Recipe.find(params[:recipe_id])
+    @foods = Food.all
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_foods = RecipeFood.new(recipe_foods_params)
+    @recipe_foods.recipe = @recipe
+    if @recipe_foods.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  def recipe_foods_params
+    params.require(:recipe_food).permit(:food_id, :quantity)
+  end
+end
