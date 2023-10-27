1b06fe4a45227730c47297e3cf506e13b5e72810
ctokoli <matumasie@gmail.com>
2023-10-27 15:38:29 +0000

    Update recipe_foods_controller.rb

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -13,12 +13,20 @@ class RecipeFoodsController < ApplicationController
     @recipe_foods = RecipeFood.new(recipe_foods_params)
     @recipe_foods.recipe = @recipe
     if @recipe_foods.save
+      flash[:notice] = 'Recipe item was created successfully'
       redirect_to recipe_path(@recipe)
     else
       render :new
     end
   end
 
+  def destroy
+    @recipe_foods = RecipeFood.find(params[:id])
+    @recipe_foods.destroy
+    flash[:notice] = 'Recipe Item was successfully deleted.'
+    redirect_to recipe_path(@recipe_foods.recipe)
+  end
+  
   def recipe_foods_params
     params.require(:recipe_food).permit(:food_id, :quantity)
   end
