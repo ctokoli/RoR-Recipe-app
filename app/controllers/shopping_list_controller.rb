cd18c6be618a81542c2aadb3de9f4e99ca0e8c92
ctokoli <matumasie@gmail.com>
2023-10-28 20:03:27 +0000

    Update shopping_list_controller.rb

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -3,10 +3,11 @@ class ShoppingListController < ApplicationController
   def index
     @recipes = Recipe.find(params[:recipe_id])
     @recipe_foods = @recipes.recipe_foods
-    @remaining_foods = remaining_foods
+    @remaining_foods_price = remaining_foods_price
+    @missing_foods = missing
   end
 
-  def remaining_foods
+  def remaining_foods_price
     total_foods = current_user.foods
     total_recipe_foods_price = 0
     total_foods_price = 0
@@ -18,4 +19,18 @@ class ShoppingListController < ApplicationController
     end
     total_foods_price - total_recipe_foods_price
   end
+
+  def missing
+    @user = current_user # Assuming you have a way to get the logged-in user
+    foods = @user.foods
+    recipe_foods = @recipes.recipe_foods
+    missing_foods = []
+    recipe_foods.each do |recipe_food|
+      foods.each do |food|
+        missing_foods << food if recipe_food.food == food
+      end
+    end
+    reamining_foods = foods - missing_foods
+  end
+
 end
