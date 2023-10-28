35ce2479a053104821e753cb5a5dc1dad54d3a67
ctokoli <matumasie@gmail.com>
2023-10-28 19:01:18 +0000

    Update shopping_list_controller.rb

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -3,5 +3,19 @@ class ShoppingListController < ApplicationController
   def index
     @recipes = Recipe.find(params[:recipe_id])
     @recipe_foods = @recipes.recipe_foods
+    @remaining_foods = remaining_foods
+  end
+
+  def remaining_foods
+    total_foods = current_user.foods
+    total_recipe_foods_price = 0
+    total_foods_price = 0
+    total_foods.each do |food|
+      total_foods_price += food.price
+    end
+    @recipe_foods.each do |recipe_food|
+      total_recipe_foods_price += recipe_food.food.price
+    end
+    remain = total_foods_price - total_recipe_foods_price
   end
 end
