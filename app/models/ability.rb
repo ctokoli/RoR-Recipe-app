aaa92883ee78fc7f6dc2c6c79368b1af1c2271df
ctokoli <matumasie@gmail.com>
2023-10-28 20:03:27 +0000

    Add ability.rb

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    can :read, Recipe, user_id: user.id
+    
+    can :manage, Recipe, user_id: user.id unless user.nil?
+    can :manage, Food, user_id: user.id unless user.nil?
+  end
+end
