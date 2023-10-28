afb8a02f28fa037140957e542226675973fec1b2
ctokoli <matumasie@gmail.com>
2023-10-28 20:03:27 +0000

    Update recipes_controller.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
   def index
     @user = current_user
     @recipes = @user.recipes.includes(:user)
